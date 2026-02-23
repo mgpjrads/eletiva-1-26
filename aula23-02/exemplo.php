5036b692bb319c94a2aa796fb7e060471f3dd13c
Marcelo Gimenes <mgpjr.ads@gmail.com>
2026-02-23 20:16:08 -0300

    arquivos php

---
+++ aula23-02/exemplo.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html lang="pt-BR">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>Primeiro exemplo PHP</title>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" >
+</head>
+<body> 
+<div class="container py-3">
+    <h1>Primeiro exemplo PHP</h1>
+    <h3>Data de hoje: <?php echo date("d/m/Y"); ?> </h3>
+    <form method="post" action="resposta.php">
+        <div class="mb-3">
+              <label for="nome" class="form-label">Informe o seu nome</label>
+              <input type="text" id="nome" name="nome" class="form-control" required="">
+        </div>
+        <button type="submit" class="btn btn-primary">Enviar</button>
+    </form>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
+</div>
+</body>
+</html>
