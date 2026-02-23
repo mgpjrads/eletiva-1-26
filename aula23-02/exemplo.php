3a845ef61b774d797a2046d93bc16eac6214bcc5
Marcelo Gimenes <mgpjr.ads@gmail.com>
2026-02-23 20:31:01 -0300

    arquivos php atualização

--- aula23-02/exemplo.php
+++ aula23-02/exemplo.php
@@ -10,13 +10,19 @@
 <div class="container py-3">
     <h1>Primeiro exemplo PHP</h1>
     <h3>Data de hoje: <?php echo date("d/m/Y"); ?> </h3>
-    <form method="post" action="resposta.php">
+    <form method="post" action="exemplo.php">
         <div class="mb-3">
               <label for="nome" class="form-label">Informe o seu nome</label>
               <input type="text" id="nome" name="nome" class="form-control" required="">
         </div>
         <button type="submit" class="btn btn-primary">Enviar</button>
     </form>
+    <?php
+        if($_SERVER['REQUEST_METHOD'] == "POST"){
+            $nome = $_POST['nome'];
+            echo "Bem vindo (a) $nome </p>";
+        }
+    ?>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
 </div>
 </body>
