ccf4cfcd93bdfab111ec3f2d25d2d9ef2fdb305d
Marcelo Gimenes <mgpjr.ads@gmail.com>
2026-02-24 21:34:13 -0300

    exercicios 2 e 3 finalizados

--- 24-02/exercicio2.php
+++ 24-02/exercicio2.php
@@ -10,7 +10,7 @@
 
 <body>
     <div class="container py-3">
-        <h1>Exercicio 1</h1>
+        <h1>Exercicio 2</h1>
         <form method="post">
             <div class="mb-3">
                 <label for="valor1" class="form-label">informe o primeiro valor: </label>
@@ -26,8 +26,8 @@
             if($_SERVER['REQUEST_METHOD'] == "POST"){
                 $valor1 = $_POST['valor1'];
                 $valor2 = $_POST['valor2'];
-                $soma = $valor1 + $valor2;
-                echo "soma é: $soma";
+                $subtracao = $valor1 - $valor2;
+                echo "A subtração é: $subtracao";
             }
         ?>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
