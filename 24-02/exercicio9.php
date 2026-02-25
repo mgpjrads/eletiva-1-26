c3a026c5e8be7c8796370f20bad5943437f833de
Marcelo Gimenes <mgpjr.ads@gmail.com>
2026-02-25 17:27:05 -0300

    iniciei o exercício 9 24-02

---
+++ 24-02/exercicio9.php
@@ -0,0 +1,34 @@
+<!doctype html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Exercicio 9</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+
+<body>
+    <div class="container py-3">
+        <h1>Exercicio 9</h1>
+        <form method="post">
+            <div class="mb-3">
+                <label for="valor1" class="form-label">Informe o raio do circulo em cm: </label>
+                <input type="number" id="valor1" name="valor1" class="form-control" required="">
+            </div>
+            <button type="submit" class="btn btn-primary">Enviar</button>
+        </form>
+        <?php
+            if($_SERVER['REQUEST_METHOD'] == "POST"){
+                $valor1 = $_POST['valor1'];
+
+                $pi = 3.14;
+                $calc = $pi * ($valor1 ** 2);
+                echo "Área do circulo: $calc cm";
+            }
+        ?>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
+    </div>
+</body>
+
+</html>
