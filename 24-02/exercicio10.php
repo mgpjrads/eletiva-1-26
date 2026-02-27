ef0056480df0e63eaecf2439fdb1cae9241f9b86
Marcelo Gimenes <mgpjr.ads@gmail.com>
2026-02-27 14:49:34 -0300

    Terminei os exercícios 9 e 10. além disso, alterei a numeração do title de alguns exercicios

--- 24-02/exercicio10.php
+++ 24-02/exercicio10.php
@@ -4,20 +4,20 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Exercicio 3</title>
+    <title>Exercicio 10</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 
 <body>
     <div class="container py-3">
-        <h1>Exercicio 3</h1>
+        <h1>Exercicio 10</h1>
         <form method="post">
             <div class="mb-3">
-                <label for="valor1" class="form-label">informe o primeiro valor: </label>
+                <label for="valor1" class="form-label">Digite o valor da largura em cm:</label>
                 <input type="number" id="valor1" name="valor1" class="form-control" required="">
             </div>
             <div class="mb-3">
-                <label for="valor2" class="form-label">informe o segundo valor: </label>
+                <label for="valor2" class="form-label">Digite o valor da altura em cm:</label>
                 <input type="number" id="valor2" name="valor2" class="form-control" required="">
             </div>
             <button type="submit" class="btn btn-primary">Enviar</button>
@@ -26,8 +26,9 @@
             if($_SERVER['REQUEST_METHOD'] == "POST"){
                 $valor1 = $_POST['valor1'];
                 $valor2 = $_POST['valor2'];
-                $multiplicacao = $valor1 * $valor2;
-                echo "O resultado é: $multiplicacao";
+
+                $perimetro = ($valor1 * 2) + ($valor2 * 2);
+                echo "O perimetro do retângulo é: $perimetro cm";
             }
         ?>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
--- 24-02/exercicio9.php
+++ 24-02/exercicio9.php
@@ -22,9 +22,9 @@
             if($_SERVER['REQUEST_METHOD'] == "POST"){
                 $valor1 = $_POST['valor1'];
 
-                $pi = 3.14;
+                $pi = 3.14159;
                 $calc = $pi * ($valor1 ** 2);
-                echo "Área do circulo: $calc cm";
+                echo "Área do circulo: $calc cm²";
             }
         ?>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
