cdb2da7684dbc07fe9132fae57b163f92567bb89
Marcelo Gimenes <mgpjr.ads@gmail.com>
2026-03-07 21:51:44 -0300

    Conclui a atividade 3: script PHP para ordenar números em ordem crescente a partir de um formulário

--- 03-03/atv3.php
+++ 03-03/atv3.php
@@ -29,11 +29,15 @@
             {
                 echo "Ordem crescente: $b, $a";
             }
-    }   elseif ($b > $a)
+      elseif ($b > $a)
             {
-                echo "Ordem crescente: $b, $a";
+                echo "Ordem crescente: $a, $b";
             }
         elseif ($a == $b)
+            {
+                echo"Números iguais: $a";
+            }
+    }        
 ?>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
 </div>
