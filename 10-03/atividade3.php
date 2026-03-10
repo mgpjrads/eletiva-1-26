88c3287cee7a763bb7c490a20deedcc2c3762378
Marcelo Gimenes <mgpjr.ads@gmail.com>
2026-03-10 20:41:52 -0300

    Conclui a atividade 3: script PHP para verificar se um texto está contido em outro

---
+++ 10-03/atividade3.php
@@ -0,0 +1,41 @@
+<!doctype html>
+<html lang="pt-BR">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>Atividade 3</title>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" >
+</head>
+<body> 
+<div class="container py-3">
+<h1>Atividade 3</h1>
+<form method="post">
+<div class="mb-3">
+              <label for="valor1" class="form-label">Digite um texto:</label>
+              <input type="text" id="valor1" name="valor1" class="form-control" required="">
+            </div><div class="mb-3">
+              <label for="valor2" class="form-label">Digite um texto:</label>
+              <input type="text" id="valor2" name="valor2" class="form-control" required="">
+            </div>
+<button type="submit" class="btn btn-primary">Enviar</button>
+</form>
+<?php
+    if ($_SERVER["REQUEST_METHOD"] == "POST")
+    {
+        $texto1 = $_POST['valor1'];
+        $texto2 = $_POST['valor2'];
+
+        if(str_contains($texto1, $texto2))
+        {
+            echo "'$texto2' está contido em '$texto1'";
+        }
+        else
+        {
+            echo "'$texto2' não está contido em '$texto1'";
+        }
+    }
+?>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
+</div>
+</body>
+</html>
