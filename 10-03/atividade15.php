ded3ecb73198fed74758752e07ce1a9800f5c80b
Marcelo Gimenes <mgpjr.ads@gmail.com>
2026-03-17 15:11:53 -0300

    Conclui a atividade 15: script PHP para apresentar o domínio de um e-mail por meio de um formulário

---
+++ 10-03/atividade15.php
@@ -0,0 +1,35 @@
+<!doctype html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Atividade 15</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+
+<body>
+    <div class="container py-3">
+        <h1>Atividade 15</h1>
+        <form method="post">
+            <div class="mb-3">
+                <label for="valor1" class="form-label">Digite um endereço de e-mail:</label>
+                <input type="text" id="valor1" name="valor1" class="form-control" required="">
+            </div>
+            <button type="submit" class="btn btn-primary">Enviar</button>
+        </form>
+        <?php
+            if($_SERVER['REQUEST_METHOD'] == "POST")
+                {
+                    $email = $_POST['valor1'];
+
+                    $dominio = substr(strrchr($email, "@"), 1);
+
+                    echo "Domínio informado: $dominio";
+                }
+        ?>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
+    </div>
+</body>
+
+</html>
