41d43bba0c545319779ce73328b9f229ade69ea6
Marcelo Gimenes <mgpjr.ads@gmail.com>
2026-04-04 13:01:27 -0300

    exibindo o mapa pelo preço conforme o enunciado

--- 17-03/exercicio04.php
+++ 17-03/exercicio04.php
@@ -84,11 +84,12 @@
                 }
 
                 $novoMapa = array_combine($produto, $novoPreco);
+                asort($novoMapa);
                 echo"<p></p>";
                 print_r($novoMapa);
                 echo"<p></p>";
                 foreach($novoMapa as $chave => $valor)
-                    echo "<p>Produto: $chave - Preço: R$ $valor</p>";
+                    echo "<p>Produto: $chave - Preço: R$". number_format($valor, 2, ",", "."). "</p>";
             }
         ?>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
