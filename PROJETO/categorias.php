f6b2b3d71eae3feb974a83206622f8d97e8918ca
Marcelo Gimenes <mgpjr.ads@gmail.com>
2026-05-05 20:02:37 -0300

    aula 05-05

--- PROJETO/categorias.php
+++ PROJETO/categorias.php
@@ -1,5 +1,12 @@
 <?php
     require_once('cabecalho.php');
+    require_once('conexao.php');
+    try{
+      $stmt = $pdo->query('SELECT * FROM categorias');
+      $resultado = $stmt->fetchAll();
+    } catch(Exception $e){
+      echo "Erro: ".$e->getMessage();
+    }
 ?>
 
 
@@ -14,51 +21,17 @@
               </tr>
             </thead>
             <tbody>
-              
+                <?php foreach ($resultado as $r): ?>
                 <tr>
-                  <td>1</td>
-                  <td>Exemplo</td>
+                  <td><?= $r['id'] ?></td> 
+                  <td><?= $r['nome'] ?></td>
                   <td class="d-flex gap-2">
                     <a href="alterar_categoria.php" class="btn btn-sm btn-warning">Editar</a>
                     <a href="consultar_categoria.php" class="btn btn-sm btn-info">Consultar</a>
                   </td>
                 </tr>
-              
-                <tr>
-                  <td>2</td>
-                  <td>Exemplo</td>
-                  <td class="d-flex gap-2">
-                    <a href="#" class="btn btn-sm btn-warning">Editar</a>
-                    <a href="#" class="btn btn-sm btn-info">Consultar</a>
-                  </td>
-                </tr>
-              
-                <tr>
-                  <td>3</td>
-                  <td>Exemplo</td>
-                  <td class="d-flex gap-2">
-                    <a href="#" class="btn btn-sm btn-warning">Editar</a>
-                    <a href="#" class="btn btn-sm btn-info">Consultar</a>
-                  </td>
-                </tr>
-              
-                <tr>
-                  <td>4</td>
-                  <td>Exemplo</td>
-                  <td class="d-flex gap-2">
-                    <a href="#" class="btn btn-sm btn-warning">Editar</a>
-                    <a href="#" class="btn btn-sm btn-info">Consultar</a>
-                  </td>
-                </tr>
-              
-                <tr>
-                  <td>5</td>
-                  <td>Exemplo</td>
-                  <td class="d-flex gap-2">
-                    <a href="#" class="btn btn-sm btn-warning">Editar</a>
-                    <a href="#" class="btn btn-sm btn-info">Consultar</a>
-                  </td>
-                </tr>
+                <?php endforeach; ?>
+                
               
             </tbody>
           </table>
