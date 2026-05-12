e63e59cb4379335bf5660bcf14525644b75ad84c
Marcelo Gimenes <mgpjr.ads@gmail.com>
2026-05-12 20:45:02 -0300

    aula12-05

---
+++ PROJETO/novo_produto.php
@@ -0,0 +1,54 @@
+<?php
+    require_once('cabecalho.php');
+    require_once('conexao.php');
+    try{
+        $stmt = $pdo->query("SELECT * FROM categorias");
+        $resultado = $stmt->fetchall();
+    }catch(exception $e){
+        die("Erro: ". $e->getMessage());
+    }
+
+?>
+
+<h1>Novo Produto</h1>
+    <form method="post">
+        <div class="mb-3">
+              <label for="nome" class="form-label">Informe a descrição</label>
+              <input type="text" id="descricao" name="descricao" class="form-control" required="">
+        </div>
+        <div class="mb-3">
+              <label for="nome" class="form-label">Informe o valor</label>
+              <input type="text" id="valor" name="valor" class="form-control" required="">
+        </div>
+        <div class="mb-3">
+              <label for="categoria" class="form-label">Selecione a categoria</label>
+              <select name="categoria" id="categoria" class="form-select">
+                <?php foreach($resultado as $r):?>
+                    <option value="<?= $r['id']?>"><?= $r['nome'] ?></option>
+                <?php endforeach; ?>
+             </select>
+        </div>
+        <button type="submit" class="btn btn-primary">Enviar</button>
+    </form>
+    <?php
+      if ($_SERVER['REQUEST_METHOD'] == 'POST'){
+        require_once('conexao.php');
+        $descricao = $_POST['descricao'];
+        $valor = $_POST['valor'];
+        $categoria = $_POST['categoria'];
+        try{
+          $stmt = $pdo->prepare('INSERT INTO produtos (nome, valor, estoque, categorias_id) VALUES (?, ?, ?, ?);');
+          if($stmt->execute([$descricao, $valor, 1, $categoria])){
+            echo "<p>Cadastro realizado!</p>";
+          } else {
+            echo "<p>Erro ao cadastrar! Tente novamente</p>";
+          }
+        } catch(Exception $e){
+          echo "Erro: ".$e->getMessage();
+        }
+      }
+    ?>
+
+<?php
+    require_once('rodape.php');
+?>
--- PROJETO/produtos.php
+++ PROJETO/produtos.php
@@ -1,9 +1,41 @@
 <?php
     require_once('cabecalho.php');
+    require_once('conexao.php');
+    try{
+        $stmt = $pdo->query('SELECT p.*, c.nome as descricao FROM produtos p INNER JOIN categorias c ON c.id = p.categorias_id');
+        $resultado = $stmt->fetchAll();
+    } catch(Exception $e){
+        echo "Erro: ".$e->getMessage();
+    }
 ?>
 
-
+<h2>Produtos</h2>
+    <a href="novo_produto.php" class="btn btn-success mb-3">Novo Registro</a>
+    <table class="table table-hover table-striped">
+    <thead>
+        <tr>
+        <th>ID</th>
+        <th>Descrição</th>
+        <th>Categoria</th>
+        <th>Ações</th>
+        </tr>
+    </thead>
+    <tbody>
+        <?php foreach ($resultado as $r): ?>
+        <tr>
+            <td><?= $r['id'] ?></td>
+            <td><?= $r['nome'] ?></td>
+            <td><?= $r['descricao'] ?></td>
+            <td class="d-flex gap-2">
+            <a href="alterar_produto.php?id=<?= $r['id'] ?>" class="btn btn-sm btn-warning">Editar</a>
+            <a href="consultar_produto.php?id=<?= $r['id'] ?>" class="btn btn-sm btn-info">Consultar</a>
+            </td>
+        </tr>
+        <?php endforeach; ?>
+    </tbody>
+    </table>
 
 <?php
     require_once('rodape.php');
+
 ?>
